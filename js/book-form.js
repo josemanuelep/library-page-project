a7fbf4aab68c4ba7b0f4a9c5ab67c088e55ee7f2
Jose Manuel <jose.echeverri@upb.edu.co>
2020-05-17 19:46:50 -0500

    Trying to pait categories

--- js/book-form.js
+++ js/book-form.js
@@ -1,45 +1,58 @@
-import { getUrlParameter } from './util/url-params.js';
-
 $(document).ready(function () {
-    console.log('Book detail js loaded!');
-    getCategories();
-    getBook();
-    $("#delete_book").click(function (e) {
-        e.preventDefault();
-        bootbox.confirm("¿Desea eliminar el libro?", function (result) {
-            if (result) {
-                deleteBook();
-                window.location.href = "http://127.0.0.1/library-page-project/#";
-            }
-        });
-
+  console.log('Book detail js loaded!');
+  getCategories();
+  getBook();
+  $("#delete_book").click(function (e) {
+    e.preventDefault();
+    bootbox.confirm("¿Desea eliminar el libro?", function (result) {
+      if (result) {
+        deleteBook();
+        window.location.href = "http://127.0.0.1/library-page-project/#";
+      }
     });
-    $("#update_book").click(function (e) {
-        e.preventDefault();
-        bootbox.confirm("¿Desea actualizar el libro?", function (result) {
-            if (result) {
-                updateBook();
 
-            }
-        });
+  });
+  $("#update_book").click(function (e) {
+    e.preventDefault();
+    bootbox.confirm("¿Desea actualizar el libro?", function (result) {
+      if (result) {
+        updateBook();
 
+      }
     });
+
+  });
 });
 
 var book = [];
 
+function getUrlParameter(sParam) {
+  var sPageURL = window.location.search.substring(1),
+    sURLVariables = sPageURL.split('&'),
+    sParameterName,
+    i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+    }
+  }
+}
 function getBook() {
-    $.ajax({
-        type: "GET",
-        url: "api/books/read_one.php?id=" + getUrlParameter('id'),
-        data: "",
-        dataType: "JSON",
-        success: function (book) {
-            let isBorrwed = book.isBorrwed==0 ? false : true;
-            console.info(isBorrwed);
-            if (isBorrwed) {
-                console.log("Painting with lend");
-                let template = ' <form>\
+  $.ajax({
+    type: "GET",
+    url: "api/books/read_one.php?id=" + getUrlParameter('id'),
+    data: "",
+    dataType: "JSON",
+    success: function (book) {
+      let isBorrwed = book.isBorrwed == 0 ? false : true;
+      console.info(book);
+      //document.getElementById("book_type").selectedIndex = book.book_type;
+      if (isBorrwed) {
+        console.log("Painting with lend");
+        let template = ' <form>\
             <hr>\
           <h4>Datos del libro</h4>\
             <div class="form-row">\
@@ -99,18 +112,18 @@ function getBook() {
               </div>\
             </div>\
           </form>';
-                $("#books_container").append(template);
-                $('#book_name').val(book.book_name);
-                $('#book_isbn').val(book.isbn);
-                $('#inputState option[value='+isBorrwed+']').attr("selected", true);
-                $("#user_name").val(book.name_user);
-                $("#user_last").val(book.last_name);
-                $("#user_doc").val(book.doc);
-                $("#book_initial_date").val(book.init_date);
-                $("#book_end_date").val(book.end_date);
-            } else {
-                console.log("Painting without lend");
-                let template = ' <form>\
+        $("#books_container").append(template);
+        $('#book_name').val(book.book_name);
+        $('#book_isbn').val(book.isbn);
+        $('#inputState option[value=' + isBorrwed + ']').attr("selected", true);
+        $("#user_name").val(book.name_user);
+        $("#user_last").val(book.last_name);
+        $("#user_doc").val(book.doc);
+        $("#book_initial_date").val(book.init_date);
+        $("#book_end_date").val(book.end_date);
+      } else {
+        console.log("Painting without lend");
+        let template = ' <form>\
             <hr>\
           <h4>Datos del libro</h4>\
             <div class="form-row">\
@@ -159,66 +172,85 @@ function getBook() {
 <br\>\
             </div>\
           </form>';
-                $("#books_container").append(template);
-                $('#book_name').val(book.book_name);
-                $('#book_isbn').val(book.isbn);
-                $('#inputState option[value='+isBorrwed+']').attr("selected", false);
-            }
+        $("#books_container").append(template);
+        $('#book_name').val(book.book_name);
+        $('#book_isbn').val(book.isbn);
+        $('#inputState option[value=' + isBorrwed + ']').attr("selected", false);
+      }
 
-        }
-    });
+    }
+  });
 }
-function getCategories(){
+function getCategories() {
+  let categories = [];
   $.ajax({
     type: "GET",
     url: "api/books/categories.php",
     data: "",
     dataType: "JSON",
     success: function (response) {
-      $.each(response, function (indexInArray, valueOfElement) { 
-        $('#book_type').append(new Option(valueOfElement.category, valueOfElement.id)); 
-         
-      });
+      categories = response;
+      console.log(categories);
+      var o = new Option("option text", "value");
+      /// jquerify the DOM object 'o' so we can use the html method
+      $(o).html("option text");
+      $("#selectList").append(o);
+    }
+  });
+  var i;
+  for (i = 0; i < categories.length; i++) {
+    // get reference to select element
+    console.log(categories.length);
+    var sel = document.getElementById("book_type");
+
+    // create new option element
+    var opt = document.createElement("option");
+
+    // create text node to add to option element (opt)
+    opt.appendChild(document.createTextNode(iterator.category));
+
+    // set value property of opt
+    opt.value = iterator.id;
+
+    // add opt to end of select box (sel)
+    sel.appendChild(opt);
+  }
+}
+function deleteBook() {
+  var id = {
+    "id": getUrlParameter('id')
+  }
+  $.ajax({
+    type: "POST",
+    url: "api/books/delete.php",
+    data: JSON.stringify(id),
+    dataType: "JSON",
+    success: function (response) {
+      console.log(response);
     }
   });
 }
-
-function deleteBook() {
-    var id = {
-        "id": getUrlParameter('id')
-    }
-    $.ajax({
-        type: "POST",
-        url: "api/books/delete.php",
-        data: JSON.stringify(id),
-        dataType: "JSON",
-        success: function (response) {
-            console.log(response);
-        }
-    });
-}
-
 function updateBook() {
-    var book = {
-        "id": getUrlParameter('id'),
-        "name": $('#book_name').val(),
-        "type": $('#book_type').val(),
-        "isbn": $('#book_isbn').val()
+  var book = {
+    "id": getUrlParameter('id'),
+    "name": $('#book_name').val(),
+    "type": $('#book_type').val(),
+    "isbn": $('#book_isbn').val()
 
+  }
+  $.ajax({
+    type: "POST",
+    url: "api/books/update.php",
+    data: JSON.stringify(book),
+    dataType: "JSON",
+    success: function (response) {
+      if (response.message == "Book was updated.") {
+        bootbox.alert("Se ha actualizado el libro!");
+      } else {
+        bootbox.alert("No se  ha actualizado el libro!, intente de nuevo");
+      }
     }
-    $.ajax({
-        type: "POST",
-        url: "api/books/update.php",
-        data: JSON.stringify(book),
-        dataType: "JSON",
-        success: function (response) {
-            if (response.message == "Book was updated.") {
-                bootbox.alert("Se ha actualizado el libro!");
-            } else {
-                bootbox.alert("No se  ha actualizado el libro!, intente de nuevo");
-            }
-        }
-    });
+  });
 }
 
 
